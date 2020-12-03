382b1017a3edfe96100f3eedb5504563abef9b9b
K-Neg <35573703+K-Neg@users.noreply.github.com>
2020-12-03 15:36:08 -0300

    Update config.h

--- lora_bme_280/Lora_receiver_ada/config.h
+++ lora_bme_280/Lora_receiver_ada/config.h
@@ -1,8 +1,7 @@
-//#define WIFI_SSID "moto"
-//#define WIFI_PASS "12345678"
-
+//
 #define WIFI_SSID "thor2"
-#define WIFI_PASS "novosol123"
+#define WIFI_PASS "alcoolgel"
 
+//AIO credencials
 #define IO_USERNAME "Kneg"
 #define IO_KEY "aio_mFYV03EtlaHbDOGPIndthd7UX9NO"
